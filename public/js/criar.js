02954e87c68c6d87b4d55b92da3c172115d7936e
moiseshbs <moises.santos@sptech.school>
2024-10-24 16:26:54 -0300

    Função para tags aparecerem, tela de perfil de usuarios e usuario proprio

--- public/js/criar.js
+++ public/js/criar.js
@@ -1,6 +1,16 @@
 var contarTags = 0;
 
+function verificarEnter(event) {
+    if (event.key === 'Enter') {
+        tags();
+    }
+}
+
 function tags() {
+    var tag = input_tag.value;
+    
+    areaTags.innerHTML += `<button onclick="" class="btnTag">${tag}</button>`;
+    
     contarTags++;
 
     span_qtdTags.innerHTML = contarTags;
