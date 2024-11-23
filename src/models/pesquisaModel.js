b21d699073015f91d41081c51edb8056e00eb485
moiseshbs <moises.santos@sptech.school>
2024-11-23 19:14:39 -0300

    Correção na curtida da publicação

--- src/models/pesquisaModel.js
+++ src/models/pesquisaModel.js
@@ -25,10 +25,16 @@ function pesquisar(texto) {
                 ON c.fkPublicacao = p.idPublicacao
             LEFT JOIN visualizacao AS v
                 ON v.fkPublicacao = p.idPublicacao
+            LEFT JOIN tag_publicacao AS tp
+                ON tp.fkPublicacao = p.idPublicacao
+			LEFT JOIN tag AS t
+				ON tp.fkTag = t.idTag
             WHERE u.nome LIKE '%${texto}%'
                 OR u.username LIKE '%${texto}%'
                 OR p.titulo LIKE '%${texto}%'
                 OR p.descricao LIKE '%${texto}%'
+                OR t.nome LIKE '%${texto}%'
+                AND p.isDeleted = false
             GROUP BY 
                 p.idPublicacao, 
                 p.fkUsuario, 
